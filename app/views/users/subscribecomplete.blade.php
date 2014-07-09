2d5d5106e3aa811c38b4daf5959cc7c66ed456c2
pitchtie14 <pitchtie@gmail.com>
2014-07-10 04:13:21 +0700

    Revert "S1_laravel-master"
    This reverts commit ca808aeef62c40bc4f352063a96493cf6b2ae8d9.

--- app/views/users/subscribecomplete.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
-    <div class="panel panel-info" >
-        <div class="panel-heading">
-           <div class="panel-title">Email Subscribe</div>
-        </div>
-        Subscribe Complete !!!
-   </div>
- </div>
